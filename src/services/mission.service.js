c9ebd8b4bf8781f3c2aad322b83604e53e767fc1
codmoni <doinghmw@gmail.com>
2024-11-10 21:00:01 +0900

    'week6:featureAndRefactor'

--- src/services/mission.service.js
+++ src/services/mission.service.js
@@ -2,6 +2,6 @@ import { addMission } from "../repositories/mission.repository.js";
 import { responseFromMission } from "../dtos/mission.dto.js";
 
 export const addMissionService = async (missionData) => {
-    const missionId = await addMission(missionData);
-    return responseFromMission({ id: missionId, ...missionData });
+  const missionId = await addMission(missionData);
+  return responseFromMission({ id: missionId, ...missionData });
 };
